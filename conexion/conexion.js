b525ea4189877a3c790275115838683d9857198e
Garcia <Garcia@LAPTOP-BFUKBITH.com>
2021-11-24 02:08:46 -0500

    despliegue a heroku

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,8 +1,8 @@
 const mysql =require('mysql')
 module.exports=()=>
 mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'dbconcesionario'
+    host:'b3bvflkhfq5arqiy75jm-mysql.services.clever-cloud.com',
+    user:'u1kj9jw8chhp2ine',
+    password:'7uydVir8bD7F7Lwurfn1',
+    database:'b3bvflkhfq5arqiy75jm'
 })
